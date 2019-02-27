f59ecd0734c19148262a278f1248c985830e1ed6
Grace <thecalliopecrashed@users.noreply.github.com>
2019-02-27 12:38:29 -0800

    Add BCrypt for password verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def user
-    @user ||= User.first.tap { |u| raise "NoAccountError" if u.nil? }
-  end
 end
