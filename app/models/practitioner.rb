6310bd4fb8fb6225cb0b5b882fbebd7970b118fc
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-10-06 15:49:48 -0400

    Ensure that a chat will be created when a new practitioenr is added

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -6,12 +6,17 @@ class Practitioner < User
   validates :type, inclusion: { in: ["Practitioner"] }
   validates :email, presence: true
 
+  after_create :create_private_message_channel
 
   def create_unread_messages
     super
     Channel.where(is_private: true, user: self.patients).map do |c|
         self.messaging_notifications.create!(channel_id: c.id, user_id: self.id, read_message_count: 0)
       end
+  end
+
+  def create_private_message_channel
+    self.channels.create!(title: "tb-expert-chat", is_private: true)
   end
 
   def get_photos
