faeb6b9ebec689773178258d5bb25011a31f715c
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-12-01 17:11:28 -0500

    Resolve issue with not displaying patients missing a day of logging if they have not yet reported

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -11,8 +11,8 @@ class Practitioner < User
   def create_unread_messages
     super
     Channel.where(is_private: true, user: self.patients).map do |c|
-        self.messaging_notifications.create!(channel_id: c.id, user_id: self.id, read_message_count: 0)
-      end
+      self.messaging_notifications.create!(channel_id: c.id, user_id: self.id, read_message_count: 0)
+    end
   end
 
   def create_private_message_channel
@@ -26,7 +26,7 @@ class Practitioner < User
   end
 
   def get_historical_photos
-    historical_photos = PhotoReport.where.not(approved: nil).joins(:daily_report).joins(:patient).where(patient: self.patients )
+    historical_photos = PhotoReport.where.not(approved: nil).joins(:daily_report).joins(:patient).where(patient: self.patients)
     return(process_photos(historical_photos))
   end
 
@@ -54,25 +54,20 @@ class Practitioner < User
     reports_per_patient = DailyReport.since_last_missing_resolution.where(patient: self.patients).group(:user_id).count()
 
     latest_resolutions.each do |resolution|
-      days_since = (DateTime.now.to_date - resolution.last_resolution.to_date).to_i
-      if (!reports_per_patient[resolution.patient_id].nil? && days_since > reports_per_patient[resolution.patient_id]  )
-        new_list.push({"patientId": resolution.patient_id})
+    days_since = (DateTime.now.to_date - resolution.last_resolution.to_date).to_i
+
+      #If there are no reports found for that patient, then default to zero instead of skipping them
+      total_reports = reports_per_patient[resolution.patient_id] || 0
+      if (days_since > total_reports)
+        new_list.push({ "patientId": resolution.patient_id })
       end
     end
 
     return(new_list)
   end
 
-  def patients_where_photo_day
-    to_send = []
-    select = self.patients.active
-    select.each do |patient|
-      list = JSON.parse(patient.medication_schedule)
-    end
-  end
-
   def tasks_completed_today
-    self.resolutions.where('resolved_at BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).count + PhotoReport.where('approval_timestamp BETWEEN ? AND ? AND practitioner_id = ?',DateTime.now.beginning_of_day, DateTime.now.end_of_day,self.id).count
+    self.resolutions.where("resolved_at BETWEEN ? AND ?", DateTime.now.beginning_of_day, DateTime.now.end_of_day).count + PhotoReport.where("approval_timestamp BETWEEN ? AND ? AND practitioner_id = ?", DateTime.now.beginning_of_day, DateTime.now.end_of_day, self.id).count
   end
 
   def summary_of_daily_medication_reporting
@@ -81,9 +76,8 @@ class Practitioner < User
 
   def available_channels
     return Channel.joins(:user)
-    .where(is_private: true, users: { organization_id: self.organization_id, type: "Patient"})
-    .or(Channel.joins(:user).where(user_id: self.id))
-    .or(Channel.joins(:user).where(is_private: false)).order(:created_at)
+             .where(is_private: true, users: { organization_id: self.organization_id, type: "Patient" })
+             .or(Channel.joins(:user).where(user_id: self.id))
+             .or(Channel.joins(:user).where(is_private: false)).order(:created_at)
   end
-
 end
