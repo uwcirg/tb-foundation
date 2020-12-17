d3b63ba8b5ee93ce88ba04ab2dab932cec432734
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-12-17 11:10:10 -0800

    Ensure development scripts are not runnable. Make patients first day a photo request each time.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -223,4 +223,10 @@ class Patient < User
   def available_channels
     return Channel.where(is_private: false).or(Channel.where(is_private: true, user_id: self.id)).order(:created_at)
   end
+
+  def add_photo_day(date = Date.today)
+    if(!self.photo_days.where(date: date).exists?)
+          self.photo_days.create!(date: date)
+    end
+  end
 end
