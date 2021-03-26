9b9d92ad3010324f668fdb04197dbc121d8c2c63
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-03-26 13:41:14 -0400

    First itteration of script developed, awaiting feedback

--- bin/backup_photos.sh
+++ bin/backup_photos.sh
@@ -1,6 +1,18 @@
-docker run -it --rm --env-file .env --entrypoint=/bin/sh minio/mc -c '
-echo "Loading Minio Settings"
-mc alias set local_minio $URL_MINIO $MINIO_ACCESS_KEY $MINIO_SECRET_KEY;
-mc ls local_minio;
-mc 
+DSTAMP=$(date +%Y.%m.%d.%H.%M.%S)
+
+TEMP_LOCATION=/tmp/mc-$DSTAMP
+BACKUP_FOLDER=/Users/kylegoodwin/Desktop/minio-test #Should I let this be an input to the script?
+FINAL_LOCATION=$BACKUP_FOLDER/mc-$DSTAMP.tgz
+
+mkdir $TEMP_LOCATION
+
+docker-compose --file=docker-compose.minio-backup.yml \
+run --rm \
+-v $TEMP_LOCATION:/output \
+mc -c '
+mc alias set local-minio http://bucket:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+mc mirror local-minio /output
 '
+tar zcvpf $FINAL_LOCATION $TEMP_LOCATION
+rm -rf $TEMP_LOCATION
+find $BACKUP_FOLDER -type f -mtime +7 -exec rm {} \;
