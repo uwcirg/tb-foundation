baef61befb7f039d166be68de511660b0a99a145
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-03-29 17:05:17 -0400

    Merge backup changes from feature/file-backup

--- bin/backup_photos.sh
+++ bin/backup_photos.sh
@@ -1,6 +1,32 @@
-docker run -it --rm --env-file .env --entrypoint=/bin/sh minio/mc -c '
-echo "Loading Minio Settings"
-mc alias set local_minio $URL_MINIO $MINIO_ACCESS_KEY $MINIO_SECRET_KEY;
-mc ls local_minio;
-mc 
+#!/bin/sh -e
+
+default_backup_folder=/srv/www/tb-files.cirg.washington.edu/docker/backups
+bin_path="$(cd "$(dirname "$0")" && pwd)"
+repo_path="${bin_path}/.."
+date_stamp=$(date +%Y.%m.%d.%H.%M.%S)
+temp_location=/tmp/mc-$date_stamp
+backup_folder=${1:-$default_backup_folder}
+backup_location=$backup_folder/mc-$date_stamp.tgz
+
+if [ ! -d $temp_location ]; then
+  mkdir -p $temp_location
+fi
+
+if [ ! -d $backup_folder ]; then
+  mkdir -p $backup_folder
+fi
+
+cd "${repo_path}"
+
+docker-compose --file=docker-compose.minio-backup.yml run --rm -v $temp_location:/output mc -c \
 '
+    mc alias set local-minio http://bucket:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+    mc mirror local-minio /output
+'
+
+tar zcvpf $backup_location -C $temp_location .
+rm -rf $temp_location
+
+#tar -C argument changes directory so that the folder is not nested within /tmp
+#may have side effect of chaning the folder (cd)
+#https://stackoverflow.com/questions/5695881/how-do-i-tar-a-directory-without-retaining-the-directory-structure
