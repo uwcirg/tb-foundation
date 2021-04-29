9132fb5dfcf4a8a99e0f5c329dae6925ef27d8e6
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-04-28 22:19:03 -0400

    Add overview of photo submissions to trail summary

--- app/serializers/trial_summary_serializer.rb
+++ app/serializers/trial_summary_serializer.rb
@@ -1,6 +1,5 @@
 class TrialSummarySerializer < ActiveModel::Serializer
 
-    attributes :patients, :photos, :adherence_summary, :site_summaries
-
+    attributes :patients, :photos, :adherence_summary, :site_summaries, :request_stats
 
 end
