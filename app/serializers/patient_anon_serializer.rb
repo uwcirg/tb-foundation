190dca8ea8568823dfac9c1b776e4c4b1f72dba5
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-05-07 23:46:14 -0400

    Add photo days back into anon patient response for debugging

--- app/serializers/patient_anon_serializer.rb
+++ app/serializers/patient_anon_serializer.rb
@@ -1,5 +1,9 @@
 class PatientAnonSerializer < ActiveModel::Serializer
     attributes :id, :organization_id, :adherence, :treatment_start
 
+    has_many :photo_days do
+      object.photo_days.order("date")
+    end
+
   end
   
