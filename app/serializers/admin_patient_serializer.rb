15ed119217f13dc6dbbc97b5696eeedef8d59881
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-06-29 11:36:57 -0400

    Hotfix add photo adherence to admin patient response

--- app/serializers/admin_patient_serializer.rb
+++ app/serializers/admin_patient_serializer.rb
@@ -1,3 +1,3 @@
 class AdminPatientSerializer < ActiveModel::Serializer
-  attributes :id, :organization_id, :adherence, :treatment_start, :days_in_treatment
+  attributes :id, :organization_id, :adherence, :photo_adherence, :treatment_start, :days_in_treatment
 end
