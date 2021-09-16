74edd41bfc30b9e7b6fac0317e4061d3b6f77560
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-09-16 13:47:54 -0700

    Add methods for counting any occurance of a symptom on a given day

--- app/models/symptom_queries.rb
+++ app/models/symptom_queries.rb
@@ -58,7 +58,7 @@ class SymptomQueries
       count(CASE WHEN s.headache THEN 1 END) as headache_count,
       count(CASE WHEN s.dizziness THEN 1 END) as dizziness_count,
       count(CASE WHEN s.other != '' THEN 1 END) as other_count
-      FROM (#{DEDUPLICATED_TESTS_REMOVED}) as s
+      FROM (#{GOOD_SYMPTOM_REPORTS}) as s
     GROUP BY s.participant_id
     SQL
 
@@ -68,6 +68,71 @@ class SymptomQueries
 
   WEEKLY_SUMMAY = <<-SQL
 SELECT *
+SQL
+
+PATIENT_DAY_COUNT = <<-SQL
+SELECT 
+s.participant_id, date_trunc('day',s.timestamp) as date,
+max(CASE WHEN s.nausea THEN 1 ELSE 0 END) as nausea_occured,
+max(CASE WHEN s.redness THEN 1 ELSE 0 END) as redness_occured,
+max(CASE WHEN s.hives THEN 1 ELSE 0 END) as hives_occured,
+max(CASE WHEN s.fever THEN 1 ELSE 0 END) as fever_occured,
+max(CASE WHEN s.appetite_loss THEN 1 ELSE 0 END) as appetite_loss_occured,
+max(CASE WHEN s.blurred_vision THEN 1 ELSE 0 END) as blurred_vision_occured,
+max(CASE WHEN s.sore_belly THEN 1 ELSE 0 END) as sore_belly_occured,
+max(CASE WHEN s.yellow_coloration THEN 1 ELSE 0 END) as yellow_coloration_occured,
+max(CASE WHEN s.difficulty_breathing THEN 1 ELSE 0 END) as difficulty_breathing_occured,
+max(CASE WHEN s.facial_swelling THEN 1 ELSE 0 END) as facial_swelling_occured,
+max(CASE WHEN s.headache THEN 1 ELSE 0 END) as headache_occured,
+max(CASE WHEN s.dizziness THEN 1 ELSE 0 END) as dizziness_occured,
+max(CASE WHEN s.other != '' THEN 1 ELSE 0 END) as other_occured
+FROM (#{GOOD_SYMPTOM_REPORTS}) as s
+GROUP BY s.participant_id, date_trunc('day',s.timestamp) 
+SQL
+
+SUMMARY_MAX_ONE = <<-SQL
+SELECT t.participant_id,
+sum(nausea_occured) as nausea_sum,
+sum(redness_occured) as reness_sum,
+sum(hives_occured) as hives_sum,
+sum(fever_occured) as fever_sum,
+sum(appetite_loss_occured) as appetite_loss_sum,
+sum(blurred_vision_occured) as blurred_vision_sum,
+sum(sore_belly_occured) as stomach_ache_sum,
+sum(yellow_coloration_occured) as yellow_coloration_sum,
+sum(difficulty_breathing_occured) as difficulty_breathing_sum,
+sum(facial_swelling_occured) as facial_swelling_sum,
+sum(headache_occured) as headache_sum,
+sum(dizziness_occured) as dizziness_sum,
+sum(other_occured) as other_sum
+FROM (#{PATIENT_DAY_COUNT}) as t
+GROUP BY t.participant_id
+SQL
+
+HAD_S_GROUPED = <<-SQL
+SELECT t.participant_id, t.date, (nausea_occured + 
+    + redness_occured 
+    + hives_occured
+    + fever_occured
+    + appetite_loss_occured
+    + blurred_vision_occured
+    + sore_belly_occured
+    + yellow_coloration_occured
+    + difficulty_breathing_occured
+    + facial_swelling_occured
+    + headache_occured
+    + dizziness_occured
+    + other_occured) > 0 as had_symptoms
+FROM (#{PATIENT_DAY_COUNT }) as t
+SQL
+
+PER_DAY_COUNT_NON_ZERO = <<-SQL
+SELECT
+t.participant_id,
+sum(CASE WHEN t.had_symptoms THEN 1 ELSE 0 END) as total_non_zero
+FROM (#{HAD_S_GROUPED}) as t
+GROUP BY t.participant_id
+
 SQL
 
   #    GROUP BY date_trunc('day', symptom_reports.created_at), symptom_reports.participant_id
@@ -81,6 +146,11 @@ SQL
     ActiveRecord::Base.connection.exec_query(sql).to_a
   end
 
+  def self.test_patient_day
+    sql = ActiveRecord::Base.sanitize_sql [PATIENT_DAY_COUNT]
+    ActiveRecord::Base.connection.exec_query(sql).to_a
+  end
+
   def self.deduped_ids
     sql = ActiveRecord::Base.sanitize_sql [DEDUP_IDS]
     array = ActiveRecord::Base.connection.exec_query(sql).to_a
@@ -89,9 +159,9 @@ SQL
   end
 
   def self.export_csv
-    file = "#{Rails.root}/tmp/symptom_count_data.csv"
+    file = "#{Rails.root}/tmp/test.csv"
 
-    sql = ActiveRecord::Base.sanitize_sql [SIMPLE_COUNT]
+    sql = ActiveRecord::Base.sanitize_sql [PER_DAY_COUNT_NON_ZERO]
     table = ActiveRecord::Base.connection.exec_query(sql)
 
     CSV.open(file, "w") do |writer|
@@ -118,3 +188,18 @@ SQL
 
   end
 end
+
+
+# nausea_occured + 
+# + redness_occured 
+# + hives_occured
+# + fever_occured
+# + appetite_loss_occured
+# + blurred_vision_occured
+# + sore_belly_occured
+# + yellow_coloration_occured
+# + difficulty_breathing_occured
+# + facial_swelling_occured
+# + headache_occured
+# + dizziness_occured
+# + other_occured
