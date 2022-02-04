c2c296400e15cbacd55e33b5204e80ac889e7153
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-02-04 14:34:38 -0500

    Update authz policy for PhotoReports so only BioEngineers can create them

--- app/controllers/v2/photo_reviews_controller.rb
+++ app/controllers/v2/photo_reviews_controller.rb
@@ -2,6 +2,7 @@ class V2::PhotoReviewsController < UserController
   before_action :snake_case_params
 
   def create
+    authorize PhotoReview
     new_review = @current_user.photo_reviews.create!(create_params)
     render(json: new_review, status: :created)
   end
@@ -12,7 +13,6 @@ class V2::PhotoReviewsController < UserController
     params.require(:photo_review).permit(:photo_report_id, :test_line_review, 
       :control_line_review, :control_color_id, :test_color_id,
       :test_strip_id,:control_color_value, :test_color_value, 
-      code_applications_attributes: [:photo_code_id]
-    )
+      code_applications_attributes: [:photo_code_id])
   end
 end
