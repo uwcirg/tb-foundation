9605f814194f106bba381c6abaaf2f6a9dedb6a4
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-02-15 13:44:01 -0800

    Fix syntax for allow_destroy on previous commit

--- app/models/photo_review.rb
+++ app/models/photo_review.rb
@@ -4,9 +4,9 @@ class PhotoReview < ApplicationRecord
     belongs_to :photo_color, optional: true
   
     has_many :code_applications, dependent: :destroy
-    has_many :photo_codes , :through => :code_applications, allow_destroy: true
+    has_many :photo_codes , :through => :code_applications
 
-    accepts_nested_attributes_for :code_applications
+    accepts_nested_attributes_for :code_applications, allow_destroy: true
   
     enum test_line_review: {
       pending: 0,
