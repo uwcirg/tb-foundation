7ab35156df783f16c0dcc19303f89cb0d34130c9
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-02-16 11:16:27 -0800

    Fix authz for PhotoReviewStats

--- app/controllers/v2/photo_review_stats_controller.rb
+++ app/controllers/v2/photo_review_stats_controller.rb
@@ -1,5 +1,6 @@
 class V2::PhotoReviewStatsController < UserController
     def show
+        authorize PhotoReviewStats
         stats = PhotoReviewStats.new(@current_user)
         render(json: stats, status: :ok)
     end
