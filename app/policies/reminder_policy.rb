9be286f09efcc3f6305194008d0f83403eb5905a
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-01 11:59:06 -0400

    Add policy for reminders to allow practitioenrs to add an appointment

--- app/policies/reminder_policy.rb
+++ app/policies/reminder_policy.rb
@@ -1,19 +1,16 @@
 class ReminderPolicy < ApplicationPolicy
-    attr_reader :user
+  attr_reader :user, :reminder
 
-    def initialize(user, reminder)
-      @user = user
-      @reminder = reminder
-    end
-  
-    def create?
-      (@user.patient? && reminder.patient.id == user.id)
-      || (@user.practitioner? && reminder.patient.organization_id == practitioner.organization_id)
-    end
-
-    def delete?
-        @user
-    end
-
+  def initialize(user, reminder)
+    @user = user
+    @reminder = reminder
   end
-  
+
+  def create?
+    (@user.patient? && @reminder.patient.id == user.id) || (@user.practitioner? && @reminder.patient.organization_id == user.organization_id)
+  end
+
+  def delete?
+    (@user.patient? && @reminder.patient.id == user.id) || (@user.practitioner? && @reminder.creator_id == @user.id)
+  end
+end
