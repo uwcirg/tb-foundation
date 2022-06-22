84018cfe39d8073f89b105323e9c85a75af20ece
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-22 18:34:32 -0400

    Cleanup PatientReminderHelper, need to test

--- app/lib/patient_reminder_helper.rb
+++ app/lib/patient_reminder_helper.rb
@@ -1,5 +1,4 @@
 class PatientReminderHelper
-
   def send_test_reminders(reminder_number)
     patients_to_notify = Patient.requested_test_not_submitted(ApplicationTime.todays_date)
 
@@ -10,40 +9,33 @@ class PatientReminderHelper
   end
 
   def send_missed_reporting_reminders
-
     patients_to_notify = Patient.includes(:patient_information, :daily_reports).active.has_not_reported_in_more_than_three_days
 
     patients_to_notify.each do |patient|
-      reminder_type = evaluate_history_and_send_reminder(patient)
-      send_reporting_reminder(patient,reminder_type) unless reminder_type == 0
+      evaluate_history_and_send_reminder(patient)
     end
-
   end
 
   private
 
-  def send_reporting_reminder(patient,type)
-    patient.send_push_to_user("No ha reportado en los últimos #{REPORTING_SEGMENTS[type-1]} días", REPORTING_BODY_STRINGS[type-1] , app_url = "/missing-reporting/#{type}", 5)
-    patient.update_number_of_missed_reporting_reminders_sent(type)
-  end
-
   def evaluate_history_and_send_reminder(patient)
+    notifier = NotifyPatient.new(patient)
+
     days_since = days_since_patients_last_report(patient)
     n_reminders_sent = patient.patient_information.reminders_since_last_report
 
     if (days_since >= 30 and n_reminders_sent < 3)
-      return 3
+      notifier.missed_thirty_days_reminder
     end
 
     if (days_since >= 7 and n_reminders_sent < 2)
-      return 2
+      notifier.missed_seven_days_reminder
     end
 
     if (days_since >= 3 and n_reminders_sent < 1)
-      return 1
+      notifier.missed_three_days_reminder
     end
 
-    return 0
   end
 
   def days_since_patients_last_report(patient)
@@ -53,7 +45,6 @@ class PatientReminderHelper
     if (not last_report.nil?)
       datetime_last_report = last_report.created_at
     end
-    days_since_last_report = (Time.now.in_time_zone("America/Argentina/Buenos_Aires").to_date - datetime_last_report.to_date).to_i
-
+    days_since_last_report = (ApplicationTime.todays_date - datetime_last_report.to_date).to_i
   end
 end
