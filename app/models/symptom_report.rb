b0be66b7919faa2438a99bd354567c98eeb74f6c
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-07-27 10:17:28 -0400

    Initalize list of locale based symtoms

--- app/models/symptom_report.rb
+++ app/models/symptom_report.rb
@@ -1,22 +1,69 @@
 class SymptomReport < ApplicationRecord
   belongs_to :daily_report, optional: true
-  belongs_to :patient, :foreign_key=> :user_id
+  belongs_to :patient, :foreign_key => :user_id
   after_commit :update_patient_stats
 
-  
   scope :has_symptom, -> { where("redness=true OR hives=TRUE OR fever=TRUE OR appetite_loss=TRUE OR blurred_vision=TRUE OR sore_belly=TRUE OR yellow_coloration=TRUE OR difficulty_breathing=TRUE OR facial_swelling=TRUE OR nausea=TRUE") }
   scope :high_alert, -> { where("hives=TRUE OR blurred_vision=TRUE OR yellow_coloration=TRUE OR difficulty_breathing=TRUE OR facial_swelling=TRUE OR ( nausea=TRUE AND nausea_rating > 6)") }
   scope :low_alert, -> { has_symptom.where.not(id: high_alert) }
 
-   def as_json(*args)
-     hash = {
-       symptomList: reported_symptoms,
-       nausea_rating: nausea_rating
-     }
+  def self.locale_symptoms(deploy_id)
+    case deploy_id
+    when "id"
+      ["redness",
+       "vertigo",
+       "yellow_coloration",
+       "confusion",
+       "blurred_vision",
+       "oliguria",
+       "appetite_loss",
+       "nausea",
+       "joint_pain",
+       "burning_or_numbness",
+       "flu_symptoms",
+       "red_urine",
+       "sleepy"]
+    else
+      ["redness",
+       "hives",
+       "fever",
+       "appetite_loss",
+       "blurred_vision",
+       "sore_belly",
+       "yellow_coloration",
+       "difficulty_breathing",
+       "facial_swelling",
+       "nausea"]
+    end
+  end
 
-     hash["otherSymptoms"] = other if other.nil? || other.empty?
-     return hash
-   end
+  def self.locale_severe_symptoms(deploy_id)
+    case deploy_id
+    when "id"
+      ["redness",
+       "vertigo",
+       "yellow_coloration",
+       "confusion",
+       "blurred_vision",
+       "oliguria"]
+    else
+      ["hives",
+       "blurred_vision",
+       "yellow_coloration",
+       "difficulty_breathing",
+       "facial_swelling"]
+    end
+  end
+
+  def as_json(*args)
+    hash = {
+      symptomList: reported_symptoms,
+      nausea_rating: nausea_rating,
+    }
+
+    hash["otherSymptoms"] = other if other.nil? || other.empty?
+    return hash
+  end
 
   def reported_symptoms
     list = %w[
@@ -40,7 +87,7 @@ class SymptomReport < ApplicationRecord
       sleepy
     ].select { |symptom| self.send(symptom) }
 
-    if(!(self.other.nil? || self.other.empty?))
+    if (!(self.other.nil? || self.other.empty?))
       list.push("other: #{self.other}")
     end
     return list
@@ -55,5 +102,4 @@ class SymptomReport < ApplicationRecord
   def update_patient_stats
     self.patient.update_stats_in_background
   end
-  
 end
