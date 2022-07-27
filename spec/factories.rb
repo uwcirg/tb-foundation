3d9e950528b8dc8c546324fcff2f9f551eae365c
Kyle Goodwin <kjgoodwins@gmail.com>
2022-07-27 14:49:29 -0400

    Feature/id symptom features (#135)
    * Initalize list of locale based symtoms
    
    * Dynamic symptom list for locales working
    
    * System for swapping locales working well
    
    * Rollback naming change for ApplicationName model
    
    * Remove tests for scope since they are behavior
    
    * Create factorybot linked testing setup for SymptomReport
    
    * Add simple tests for severe symptoms based on locale
    
    Co-authored-by: Kyle Goodwin <kylegoodwin@users.noreply.github.com>

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     patient { nil }
   end
 
-
   # factory :patient_information do
   #   datetime_patient_activated {Time.now}
   # end
@@ -16,7 +15,6 @@ FactoryBot.define do
     clicked { false }
     clicked_at { "2021-05-03 17:09:08" }
   end
-
 
   orgs_array = ["Test Organization", "UW"]
 
@@ -48,4 +46,25 @@ FactoryBot.define do
     treatment_start { Faker::Date.between(from: 1.month.ago, to: Date.today) }
   end
 
+  factory :daily_report, class: DailyReport do
+    date { Faker::Date.backward(days: 14) }
+  end
+
+  factory :symptom_report, class: SymptomReport do
+    association :patient, factory: :patient_with_organization
+  end
+
+  factory :patient_with_organization, class: Patient do
+    association :organization, factory: :organization_with_practitioners
+    given_name { Faker::Name.first_name }
+    family_name { Faker::Name.last_name }
+    phone_number { Faker::Number.number(digits: 9).to_s }
+    password_digest { BCrypt::Password.create("password") }
+    treatment_start { Faker::Date.between(from: 1.month.ago, to: Date.today) }
+  end
+
+  factory :organization_with_practitioners, class: Organization do
+    title { Faker::University.name }
+    practitioners { build_list :practitioner, 1 }
+  end
 end
