0536069137df89b9d833f4edb460648f37738a5f
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-11-07 17:56:05 -0500

    Add backup step to deploy script

--- bin/deploy_with_migrations.sh
+++ bin/deploy_with_migrations.sh
@@ -2,6 +2,11 @@
 bin_path="$(cd "$(dirname "$0")" && pwd)"
 repo_path="${bin_path}/.."
 
+#Backup DB to Kyles home folder
+echo "🚧 Making a database backup..."
+mkdir -p "/home/kjgood1/deploy-backups"
+backup.sh -b "/home/kjgood1/deploy-backups/"
+
 #docker-compose commands must be run in the same directory as docker-compose.yaml
 cd "${repo_path}"
 
@@ -16,4 +21,6 @@ docker-compose exec web bash -c "bundle exec rails db:migrate"
 
 #Rails services need to be restarted for new database columns to be reflected in the application
 echo "Restarting services..."
-docker-compose restart web sidekiq;
+docker-compose stop web sidekiq;
+docker-compose rm web sidekiq;
+docker-compose up -d;
