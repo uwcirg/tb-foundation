fe642f2337da26c04765204745b558fa0e7e7916
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-11-14 20:11:47 -0500

    Hotfix for deploy time zone env config

--- app/models/application_time.rb
+++ app/models/application_time.rb
@@ -1,11 +1,15 @@
 class ApplicationTime
-    
-    def self.time_zone
-        ENV["DEPLOY_TIME_ZONE"] || "America/Argentina/Buenos_Aires"
+  def self.time_zone
+    Rails.cache.fetch("time_zone", expires_in: 1.days) do
+      env_value = ENV["DEPLOY_TIME_ZONE"]
+      if env_value.presence && TZInfo::Timezone.all_country_zone_identifiers.include?(env_value)
+        return env_value
+      end
+      "America/Argentina/Buenos_Aires"
     end
+  end
 
-    def self.todays_date
-        Time.now.in_time_zone(time_zone).to_date
-    end
-
+  def self.todays_date
+    Time.now.in_time_zone(time_zone).to_date
+  end
 end
