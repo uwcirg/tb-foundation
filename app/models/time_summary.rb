2b4f9247f7426469c90182064e45f50ff2b2a984
johnny2smooth <johnbp13@gmail.com>
2022-12-29 11:57:39 -0800

    testing ability to commit

--- app/models/time_summary.rb
+++ app/models/time_summary.rb
@@ -1,14 +1,14 @@
 class TimeSummary < ActiveModelSerializers::Model
   def initialize(number_of_days = nil)
     if (number_of_days.nil?)
-      number_of_days = (Time.now.in_time_zone("America/Argentina/Buenos_Aires").to_date - PatientInformation.order(:datetime_patient_activated).first.datetime_patient_activated.to_date).to_i
+      number_of_days = 30
     end
     @number_of_days = number_of_days 
   end
 
   def photo_reports
     {
-      requested: PhotoDay.requested.where(patient: Patient.non_test.active).where(date: Time.now() -30.days..Time.now).count,
+      requested: PhotoDay.requested.where(patient: Patient.non_test.active).where(date: Time.now() - @number_of_days.days..Time.now).count,
       submitted: submitted_photos.count,
       skipped: submitted_photos.where(photo_was_skipped: true).count,
     }
@@ -24,13 +24,13 @@ class TimeSummary < ActiveModelSerializers::Model
   end
 
   def number_of_symptoms
-    SymptomReport.has_symptom.where(daily_report_id: DailyReport.where(patient: Patient.non_test).where(date: Time.now() -30.days..Time.now)).count
+    SymptomReport.has_symptom.where(daily_report_id: DailyReport.where(patient: Patient.non_test).where(date: Time.now() - @number_of_days.days..Time.now)).count
   end
 
   private
 
   def start_date
-    Time.now.in_time_zone("America/Argentina/Buenos_Aires").to_date - @number_of_days
+    Time.now() - @number_of_days.days
   end
   
   def number_of_reports_requested
@@ -38,16 +38,16 @@ class TimeSummary < ActiveModelSerializers::Model
   end
 
   def submitted_photos
-    PhotoReport.where(daily_report_id: DailyReport.where(patient: Patient.non_test.active).where(date: Time.now() -30.days..Time.now))
+    PhotoReport.where(daily_report_id: DailyReport.where(patient: Patient.non_test.active).where(date: Time.now() -@number_of_days.days..Time.now))
   end
 
   def submitted_reports
-    DailyReport.where(patient: Patient.non_test.active).where(date: Time.now() -30.days..Time.now)
+    DailyReport.where(patient: Patient.non_test.active).where(date: Time.now() -@number_of_days.days..Time.now)
   end
 
   def all_symptoms
     symptoms_hash = {};
-    SymptomReport.has_symptom.where(daily_report_id: DailyReport.where(patient: Patient.non_test).where(date: Time.now() -30.days..Time.now)).each { |report| report.reported_symptoms.each {|symptom| if symptoms_hash.include?(symptom); symptoms_hash[symptom] += 1; else symptoms_hash[symptom] = 1; end } }
+    SymptomReport.has_symptom.where(daily_report_id: DailyReport.where(patient: Patient.non_test).where(date: Time.now() -@number_of_days.days..Time.now)).each { |report| report.reported_symptoms.each {|symptom| if symptoms_hash.include?(symptom); symptoms_hash[symptom] += 1; else symptoms_hash[symptom] = 1; end } }
     symptoms_hash;
   end
   
